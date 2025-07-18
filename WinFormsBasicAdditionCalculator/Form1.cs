0e59cb89411bb16d8011bb5c0772c57163c0dd9d
Kendall Dawson <lilkenny642@gmail.com>
2025-07-17 22:46:47 -0700

    Add project files.

---
+++ WinFormsBasicAdditionCalculator/Form1.cs
@@ -0,0 +1,10 @@
+namespace WinFormsBasicAdditionCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
